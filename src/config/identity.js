cfb281aabf0a4561969697143d01fd97fda9842a
Kojin Fox <kojinfox@gmail.com>
2025-02-07 17:52:36 -0800

    Fix CORS and script loading issues for Netlify Identity

--- src/config/identity.js
+++ src/config/identity.js
@@ -1,11 +1,28 @@
 import netlifyIdentity from 'netlify-identity-widget';
 
-// Configure the widget
-netlifyIdentity.init({
-  logo: false,
-  cookieSecure: true,
-  secureCookie: true,
-  sameSiteCookie: 'Strict'
-});
+// Wait for the script to be ready
+const initIdentity = () => {
+  return new Promise((resolve) => {
+    if (window.netlifyIdentity) {
+      netlifyIdentity.init({
+        logo: false,
+        cookieSecure: true,
+        secureCookie: true
+      });
+      resolve();
+    } else {
+      document.addEventListener('netlifyIdentityReady', () => {
+        netlifyIdentity.init({
+          logo: false,
+          cookieSecure: true,
+          secureCookie: true
+        });
+        resolve();
+      });
+    }
+  });
+};
+
+initIdentity();
 
 export default netlifyIdentity; 
